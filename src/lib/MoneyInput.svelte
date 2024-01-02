e9433c470b4e3eaf034aebc7513804ad80687d7c
mtsuts <mariamtsuts@gmail.com>
2024-01-02 23:11:07 +0400

    added main features

---
+++ src/lib/MoneyInput.svelte
@@ -0,0 +1,13 @@
+<script>
+  // @ts-nocheck
+  export let onInputChange;
+</script>
+
+<div>
+  <input
+    type="number"
+    required
+    on:input={(event) => onInputChange(event.target.value)}
+    class="mt-4 w-1/2 bg-white border-orange-300 shadow-2xl border-2 rounded-md focus-visible:outline-none"
+  />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,87 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  // @ts-nocheck
+  import MoneyInput from "$lib/MoneyInput.svelte";
+  import { inputVal } from "../store";
+  let inputValue = 0;
+  let show = false;
+  let inputValues = [];
+  let result = 0;
+  let isResult = false;
+
+  $: inputAmount = Array.from({ length: inputValue }, (_, index) => index);
+  const handleMoneyInputChange = (index, value) => {
+    inputValues[index] = value;
+  };
+
+  function calculate() {
+    console.log(inputValues);
+    console.log(inputValues.length);
+    isResult = true;
+    const max = Math.max(...inputValues);
+    const min = Math.min(...inputValues);
+    result = (max - min) / inputValues.length;
+  }
+
+  $: $inputVal = inputValue;
+  $: if (inputValue > 0) {
+    show = true;
+  } else {
+    show = false;
+  }
+  $: result;
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Sharing-calc" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div
+  class="md:p-10 p-6 text-center text-4xl text-white bg-orange-300 mt-4 rounded-xl md:mx-40 shadow-2xl"
+>
+  <div class="flex items-center justify-center">
+    <img src="/owl-svg-logo.svg" alt="owl-logo" />
+    <p>ღალტერია</p>
+  </div>
+</div>
+<div class="mt-20 text-center md:mx-40">
+  <div class="flex flex-col items-center">
+    <label for="input"> რამდენი ჯგუფი/ადამიანი იყავით?</label>
+    <input
+      type="number"
+      bind:value={inputValue}
+      required
+      class="mt-4 w-1/2 md:w-1/6 bg-white border-orange-300 shadow-2xl border-2 rounded-md focus-visible:outline-none"
+    />
+    <p class="mt-4">
+      სულ იყავით: {inputValue === null || inputValue === 0 ? 0 : inputValue}
+    </p>
+    {#if show}
+      <p class="mt-4">შეიყვანე თანხები</p>
+      <div class="grid grid-cols-2 items-center">
+        {#each inputAmount as index}
+          <MoneyInput
+            onInputChange={(value) => handleMoneyInputChange(index, value)}
+          />
+        {/each}
+      </div>
+      <button
+        on:click={() => {
+          calculate();
+        }}
+        class="mt-4 bg-gray-500 text-center p-1 rounded-lg text-white shadow-2xl"
+      >
+        დაიანგარიშე</button
+      >
+      {#if isResult}
+        <p class="mt-6">
+          ვინც ნაკლები გადაიხადა, მან უნდა გადაურიცხოს <span
+            class="text-2xl bg-gray-300 rounded-lg p-2 text-white"
+          >
+            {result}
+          </span> მას, ვინც მეტი გადაიხადა, რათა გათანაბრდეს ხარჯები
+        </p>
+      {/if}
+    {/if}
+  </div>
+</div>
